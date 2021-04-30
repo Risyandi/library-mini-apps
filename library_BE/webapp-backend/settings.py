71e9c804fd3ef7757639314616b704011dce397f
Risyandi <Risyandi.94@gmail.com>
2021-04-30 22:37:13 +0700

    fix: adding api and rest_framework

--- library_BE/webapp-backend/settings.py
+++ library_BE/webapp-backend/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'api', #define to api folder 
+    'rest_framework', #define rest-api library
 ]
 
 MIDDLEWARE = [
