755e92c2124ab5269d505c5b4ede885a880941d9
Risyandi <Risyandi.94@gmail.com>
2021-04-30 21:46:58 +0700

    fix: renaming folder

--- library_BE/webapp-backend/asgi.py
+++ library_BE/webapp-backend/asgi.py
@@ -1,5 +1,5 @@
 """
-ASGI config for library_BE project.
+ASGI config for webapp-backend project.
 
 It exposes the ASGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.asgi import get_asgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_BE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp-backend.settings')
 
 application = get_asgi_application()
--- library_BE/webapp-backend/wsgi.py
+++ library_BE/webapp-backend/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for library_BE project.
+WSGI config for webapp-backend project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'library_BE.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'webapp-backend.settings')
 
 application = get_wsgi_application()
