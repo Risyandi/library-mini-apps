db074781650984230b4276369bf1b39749ed6c54
Risyandi <Risyandi.94@gmail.com>
2021-05-01 16:41:09 +0700

    feat: adding view for buku

--- library_BE/api/views.py
+++ library_BE/api/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 from django.http import JsonResponse
+from django.contrib.auth.models import User
 from rest_framework.decorators import api_view, permission_classes
-from rest_framework import permissions
+from rest_framework import permissions, viewsets
+from . import serializer
+from .models import Buku, Kategori, Pelanggan, Transaksi
+
+# Create view response Users api
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = serializer.UserSerializer
+
+# Create view response Buku api
+class BukuViewSet(viewsets.ModelViewSet):
+    queryset = Buku.objects.all()
+    serializer_class = serializer.BukuSerializer
 
 # Create views response welcome to page api
 @api_view(["GET"])
