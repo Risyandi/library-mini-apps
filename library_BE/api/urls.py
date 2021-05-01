4f42f1142ba48efc0a7666552794121a0bf9b70a
Risyandi <Risyandi.94@gmail.com>
2021-05-01 12:45:54 +0700

    feat: adding api users

--- library_BE/api/urls.py
+++ library_BE/api/urls.py
@@ -1,6 +1,26 @@
 from django.urls import include, path
+from django.contrib.auth.models import User
+from rest_framework import serializers, viewsets, routers
 from . import views
 
+# Serializers define the API representation.
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'username', 'email', 'is_staff']
+
+# ViewSets define the view behavior.
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+# Routers provide a way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'users', UserViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
 urlpatterns = [
-    path('v1', views.welcome)
+    path('v1/', include(router.urls)),
+    path('', views.welcome),
 ]
