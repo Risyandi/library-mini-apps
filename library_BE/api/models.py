03a4515a445af78db29e6a19a2618b076ce548a2
Risyandi <Risyandi.94@gmail.com>
2021-05-01 16:39:40 +0700

    feat: adding new model for pelanggan,buku,kategori,transaksi

--- library_BE/api/models.py
+++ library_BE/api/models.py
@@ -1,3 +1,47 @@
 from django.db import models
 
-# Create your models here.
+# Create models pelanggan
+class Pelanggan(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    address = models.CharField(max_length=100)
+    handphone = models.CharField(max_length=50)
+    email = models.CharField(max_length=50)
+    join_date = models.CharField(max_length=25)
+    end_date = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = 'pelanggan'
+        verbose_name = "pelanggan"
+
+# Create models buku
+class Buku(models.Model):
+    title = models.CharField(max_length=100)
+    isbn_code = models.CharField(max_length=20)
+    author = models.CharField(max_length=25)
+    publisher = models.CharField(max_length=30)
+    year_publish = models.CharField(max_length=25)
+
+    class Meta:
+        db_table = 'buku'
+        verbose_name = "buku"
+
+# Create models kategori buku
+class Kategori(models.Model):
+    isbn_code = models.CharField(max_length=20)
+    title = models.CharField(max_length=30)
+    name_category = models.CharField(max_length=30)
+    
+    class Meta:
+        db_table = 'kategori'
+        verbose_name = "kategori buku"
+
+# Create models kategori transaksi peminjaman
+class Transaksi(models.Model):
+    start_date_borrow = models.CharField(max_length=25)
+    end_date_borrow = models.CharField(max_length=25)
+    date_return = models.CharField(max_length=25)
+    
+    class Meta:
+        db_table = 'transaksi'
+        verbose_name = "transaksi buku"
