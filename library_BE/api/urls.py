0f8391589c1a7333a2bedd7a777263fcae9f3802
Risyandi <Risyandi.94@gmail.com>
2021-05-01 19:24:30 +0700

    feat: adding new urls for api

--- library_BE/api/urls.py
+++ library_BE/api/urls.py
@@ -5,7 +5,10 @@ from . import views, serializer
 # Routers provide a way of automatically determining the URL conf.
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
-router.register(r'bukus', views.BukuViewSet)
+router.register(r'buku', views.BukuViewSet)
+router.register(r'kategori', views.KategoriViewSet)
+router.register(r'pelanggan', views.PelangganViewSet)
+router.register(r'transaksi', views.TransaksiViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
