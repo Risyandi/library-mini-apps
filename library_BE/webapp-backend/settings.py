e6fb928efb37323d5181fa6bb6586d7f4dbf0238
Risyandi <Risyandi.94@gmail.com>
2021-05-01 21:14:24 +0700

    test: adding settings auth rest but temporary comment

--- library_BE/webapp-backend/settings.py
+++ library_BE/webapp-backend/settings.py
@@ -138,5 +138,10 @@ REST_FRAMEWORK = {
     # or allow read-only access for unauthenticated users.
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
-    ]
+    ],
+    # 'DEFAULT_AUTHENTICATION_CLASSES': ( 
+    #     'rest_framework.authentication.BasicAuthentication',
+    #     'rest_framework.authentication.SessionAuthentication',
+    #     'rest_framework.authentication.TokenAuthentication',
+    # ),
 }
