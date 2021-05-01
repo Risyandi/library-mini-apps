c085caf5cf0ece2bdf7869aa4a9c981a765686df
Risyandi <Risyandi.94@gmail.com>
2021-05-01 16:40:12 +0700

    feat: register models to admin

--- library_BE/api/admin.py
+++ library_BE/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Buku,Kategori,Pelanggan,Transaksi
 
 # Register your models here.
+allModels = [Buku, Kategori, Pelanggan, Transaksi]  # iterate list of models
+admin.site.register(allModels)
