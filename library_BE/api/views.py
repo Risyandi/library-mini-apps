fd6adf02bc051497bfa3bb0286caaef8a16c75fd
Risyandi <Risyandi.94@gmail.com>
2021-05-02 12:54:40 +0700

    fix: update permission on views

--- library_BE/api/views.py
+++ library_BE/api/views.py
@@ -6,27 +6,35 @@ from rest_framework import permissions, viewsets
 from . import serializer
 from .models import Buku, Kategori, Pelanggan, Transaksi
 
+# ========= if use open api using jwt activate this comment
+# @permission_classes((permissions.IsAuthenticated,))
+
 # Create view response Users api
+@permission_classes((permissions.AllowAny,))
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = serializer.UserSerializer
 
 # Create view response Buku api
+@permission_classes((permissions.AllowAny,))
 class BukuViewSet(viewsets.ModelViewSet):
     queryset = Buku.objects.all()
     serializer_class = serializer.BukuSerializer
 
 # Create view response Kategori Buku api
+@permission_classes((permissions.AllowAny,))
 class KategoriViewSet(viewsets.ModelViewSet):
     queryset = Kategori.objects.all()
     serializer_class = serializer.KategoriSerializer
 
 # Create view response Pelanggan api
+@permission_classes((permissions.AllowAny,))
 class PelangganViewSet(viewsets.ModelViewSet):
     queryset = Pelanggan.objects.all()
     serializer_class = serializer.PelangganSerializer
 
 # Create view response Pelanggan api
+@permission_classes((permissions.AllowAny,))
 class TransaksiViewSet(viewsets.ModelViewSet):
     queryset = Transaksi.objects.all()
     serializer_class = serializer.TransaksiSerializer
