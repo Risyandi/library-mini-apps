7eb9f91627bef321562f21b4ec45b64e4a145fa8
Risyandi <Risyandi.94@gmail.com>
2021-05-05 13:58:59 +0700

    fix: adding new routes

--- library_FE/src/routes.js
+++ library_FE/src/routes.js
@@ -5,7 +5,10 @@
 
 import React from 'react';
 
-const Homepage = React.lazy(() => import('./Home'))
+const Homepage = React.lazy(() => import('./pages/Home'))
+const PelangganPage = React.lazy(() => import('./pages/Pelanggan'))
+const BukuPage = React.lazy(() => import('./pages/Buku'))
+const TransaksiPage = React.lazy(() => import('./pages/Transaksi'))
 
 const routes = [
     {
@@ -14,7 +17,28 @@ const routes = [
         name: 'Homepage',
         component: Homepage,
         typeLayout: 'full-layout'
-    }
+    },
+    {
+        path: '/create/pelanggan',
+        exact: true,
+        name: 'Create Pelanggan Page',
+        component: PelangganPage,
+        typeLayout: 'full-layout'
+    },
+    {
+        path: '/create/buku',
+        exact: true,
+        name: 'Create Buku Page',
+        component: BukuPage,
+        typeLayout: 'full-layout'
+    },
+    {
+        path: '/create/transaksi',
+        exact: true,
+        name: 'Create Transaksi Page',
+        component: TransaksiPage,
+        typeLayout: 'full-layout'
+    },
 ]
 
 export default routes;
