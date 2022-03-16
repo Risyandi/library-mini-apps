23e79e0015afeacd0484cc39cabd9e6de0f7f0f3
Risyandi <risyandi.94@gmail.com>
2022-03-16 21:15:52 +0700

    fix: adding url allows cors

--- library_BE/webapp-backend/settings.py
+++ library_BE/webapp-backend/settings.py
@@ -167,6 +167,7 @@ REST_FRAMEWORK = {
 
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
+    "http://localhost:3002",
     "http://localhost:8000",
     "http://127.0.0.1:8000",
     "http://127.0.0.1:3000",
